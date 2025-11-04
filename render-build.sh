db4fecb891cf548c07914b498a111aaec6a7fa85
nishayadav9 <csdt.it@gmail.com>
2025-11-04 11:22:35 +0530

    Added loaddata command to render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input 
 python manage.py makemigrations --no-input 
 python manage.py migrate --no-input 
+python manage.py loaddata data.json
