6cc4397d8e5f492fd9406bbea58f2269dbe76fcb
nishayadav9 <csdt.it@gmail.com>
2025-11-04 14:53:55 +0530

    Force add media images

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'product', 'quantity', 'total_price', 'ordered_at', 'shipping_address')
-    list_filter = ('ordered_at',)
-    search_fields = ('user__email', 'product__name', 'shipping_address__name')
